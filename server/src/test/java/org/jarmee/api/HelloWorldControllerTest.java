eef2e1f3e3a445e3a9e7529f91ebd1d591a469fd
Jari Möllenbernd <jari@moellenbernd.de>
2018-03-25 23:10:47 +0200

    Added integration test for HelloWorldController

--- server/src/test/java/org/jarmee/api/HelloWorldControllerTest.java
+++ server/src/test/java/org/jarmee/api/HelloWorldControllerTest.java
@@ -1,4 +1,4 @@
-package org.jarmee;
+package org.jarmee.api;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -7,11 +7,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-public class HelloWorldControllerIT {
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloWorldControllerTest {
 
     @Autowired
     MockMvc mockMvc;
